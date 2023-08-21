c85e311f2f82b03788363809e32b6227dc05d6d3
mohamedNaser1212 <nasergamal33e@gmail.com>
2023-08-22 00:53:10 +0300

    background and logo done

--- lib/screens/Loginscreen.dart
+++ lib/screens/Loginscreen.dart
@@ -1,160 +1,148 @@
+import 'package:flutter/material.dart';
 import 'package:alex_uni_app/constants.dart';
 import 'package:alex_uni_app/screens/login_details.dart';
 import 'package:alex_uni_app/screens/regesteration_form.dart';
-import 'package:flutter/material.dart';
 
 import '../custom_widgets/customORwidget.dart';
 import '../custom_widgets/custom_social_media_icons.dart';
 
 class LoginScreen extends StatelessWidget {
-  const LoginScreen({super.key});
-  static String id = 'LoginScreen';
+  static  String id = 'LoginScreen';
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: Center(
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Image.asset(
-                      'assets/logo.png',
-                      height: 200,
-                      width: 300,
-                    ),
-                  ],
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                 Column(
-                  children: [
-                const  Text(
-                  'WELCOME TO ',
-                  style: TextStyle(
-                      fontSize: 34,
-                      fontWeight: FontWeight.w300,
-                      fontFamily: 'Bebas Neue',
-                      color: KBlueColor),
-                   textAlign: TextAlign.center,
-                ),
-               const  Text(
-                  'ALEXANDRIA UNIVERSITY APP. ',
-                  style: TextStyle(
-                      fontSize: 34,
-                      fontFamily: 'Bebas Neue',
-                      fontWeight: FontWeight.w300,
-                      color: KBlueColor),
-                  textAlign: TextAlign.center,
-
-                ),
-                   const SizedBox(
-                      height: 40,
-                    ),
-                    GestureDetector(
-                      onTap: (){
-                        Navigator.pushNamed(context,   LoginDetails.id);
-
-                      },
-                      child: Container(
-                        width: 200,
-                        height: 60,
-                        decoration:const  BoxDecoration(
-                          borderRadius: BorderRadius.only(
-                            topLeft: Radius.circular(90),
-                            bottomRight: Radius.circular(50),
-                            topRight: Radius.circular(50),
-                          ),
-                          color: KLOGINBUTTONCOLOR,
-
+    return Scaffold(
+      body: Container(
+        // Set the background image using BoxDecoration
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/whitebackground.png'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: SafeArea(
+          child: Center(
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.asset('assets/facebook 4.png'),
+                    ],
+                  ),
+                  const SizedBox(height: 20),
+                  Column(
+                    children: [
+                      const Text(
+                        'WELCOME TO ',
+                        style: TextStyle(
+                          fontSize: 34,
+                          fontWeight: FontWeight.w300,
+                          fontFamily: 'Bebas Neue',
+                          color: KBlueColor,
                         ),
-                        child: const Center(
-                          child: Text('تسجيل الدخول',
-                            style: TextStyle(
+                        textAlign: TextAlign.center,
+                      ),
+                      const Text(
+                        'ALEXANDRIA UNIVERSITY APP. ',
+                        style: TextStyle(
+                          fontSize: 34,
+                          fontFamily: 'Bebas Neue',
+                          fontWeight: FontWeight.w300,
+                          color: KBlueColor,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                      const SizedBox(height: 40),
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.pushNamed(context, LoginDetails.id);
+                        },
+                        child: Container(
+                          width: 200,
+                          height: 60,
+                          decoration: const BoxDecoration(
+                            borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(90),
+                              bottomRight: Radius.circular(50),
+                              topRight: Radius.circular(50),
+                            ),
+                            color: KLOGINBUTTONCOLOR,
+                          ),
+                          child: const Center(
+                            child: Text(
+                              'تسجيل الدخول',
+                              style: TextStyle(
                                 fontSize: 26,
                                 fontFamily: 'Bebas Neue',
                                 fontWeight: FontWeight.bold,
-                                color: Colors.white),
-                            textAlign: TextAlign.center,
+                                color: Colors.white,
+                              ),
+                              textAlign: TextAlign.center,
+                            ),
                           ),
-                        )
+                        ),
                       ),
-                    ),
-                    const SizedBox(
-                      height: 40,
-                    ),
-                    GestureDetector(
-                      onTap: (){
-                        Navigator.pushNamed(context,   RegesterationForm.id);
-
-                      },
-                      child: Container(
+                      const SizedBox(height: 40),
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.pushNamed(context, RegesterationForm.id);
+                        },
+                        child: Container(
                           width: 200,
                           height: 60,
-                          decoration:const  BoxDecoration(
+                          decoration: const BoxDecoration(
                             borderRadius: BorderRadius.only(
                               topLeft: Radius.circular(50),
                               bottomRight: Radius.circular(90),
                               bottomLeft: Radius.circular(50),
                             ),
                             color: KRegesterButtoncolor,
-
                           ),
                           child: const Center(
-                            child: Text('انشاء حساب',
+                            child: Text(
+                              'انشاء حساب',
                               style: TextStyle(
-                                  fontSize: 26,
-                                  fontFamily: 'Bebas Neue',
-                                  fontWeight: FontWeight.bold,
-                                  color: KBlueColor),
-                              textAlign: TextAlign.center,
-                            ),
-                          )
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    Container(
-                        width: 150,
-                        height: 60,
-                        decoration:const  BoxDecoration(
-                          borderRadius: BorderRadius.only(
-                            topLeft: Radius.circular(190),
-                            bottomRight: Radius.circular(190),
-                            bottomLeft: Radius.circular(190),
-                            topRight: Radius.circular(190),
-                          ),
-                          color: Colors.white,
-
-                        ),
-                        child: const Center(
-                          child: Text('الدخول كزائر',
-                            style: TextStyle(
                                 fontSize: 26,
                                 fontFamily: 'Bebas Neue',
                                 fontWeight: FontWeight.bold,
-                                color: KBlueColor),
+                                color: KBlueColor,
+                              ),
+                              textAlign: TextAlign.center,
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      Container(
+                        width: 150,
+                        height: 60,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(190),
+                          color: Colors.white,
+                        ),
+                        child: const Center(
+                          child: Text(
+                            'الدخول كزائر',
+                            style: TextStyle(
+                              fontSize: 26,
+                              fontFamily: 'Bebas Neue',
+                              fontWeight: FontWeight.bold,
+                              color: KBlueColor,
+                            ),
                             textAlign: TextAlign.center,
                           ),
-                        )
-                    ),
-                     const SizedBox(
-                       height:20,
-                     ),
-                   const CustomORWidget()
-
-                  ],
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                const CustomSocialMediaIcons(),
-              ],
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      const CustomORWidget(),
+                    ],
+                  ),
+                  const SizedBox(height: 20),
+                  const CustomSocialMediaIcons(),
+                ],
+              ),
             ),
           ),
         ),
@@ -162,4 +150,3 @@ class LoginScreen extends StatelessWidget {
     );
   }
 }
-
--- lib/screens/login_details.dart
+++ lib/screens/login_details.dart
@@ -1,175 +1,170 @@
-import 'package:alex_uni_app/custom_widgets/customORwidget.dart';
-import 'package:alex_uni_app/screens/Loginscreen.dart';
-import 'package:alex_uni_app/screens/login_form.dart';
-import 'package:alex_uni_app/screens/second_login_form.dart';
 import 'package:flutter/material.dart';
-
+import 'package:alex_uni_app/custom_widgets/customORwidget.dart';
+import 'package:alex_uni_app/screens/second_login_form.dart';
+import 'package:alex_uni_app/screens/login_form.dart';
 import '../constants.dart';
 import '../custom_widgets/custom_social_media_icons.dart';
 
 class LoginDetails extends StatelessWidget {
-  const LoginDetails({super.key});
-  static String id = 'login_details';
+  static  String id = 'login_details';
+
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: Center(
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Image.asset(
-                      'assets/logo.png',
-                      height: 200,
-                      width: 300,
-                    ),
-                  ],
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                Column(children: [
-                  const Text(
-                    'تسجيل الدخول ',
-                    style: TextStyle(
-                        fontSize: 34,
-                        fontWeight: FontWeight.w900,
-                        fontFamily: 'Tajawal',
-                        color: KBlueColor),
-                    textAlign: TextAlign.center,
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/whitebackground.png'), // Replace with your background image path
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: SafeArea(
+          child: Center(
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.asset(
+                        'assets/facebook 4.png',
+                        height: 200,
+                        width: 300,
+                      ),
+                    ],
                   ),
-                  const Text(
-                    'من خلال ',
-                    style: TextStyle(
-                        fontSize: 26,
-                        fontFamily: 'Tajawal',
-                        fontWeight: FontWeight.w900,
-                        color: KBlueColor),
-                    textAlign: TextAlign.center,
-                  ),
-                  const SizedBox(
-                    height: 40,
-                  ),
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.pushReplacementNamed(context, SecondLoginForm.id);
-                    },
-                    child: Container(
-                        width: 270,
-                        height: 60,
-                        decoration: const BoxDecoration(
-                          borderRadius: BorderRadius.only(
-                            topLeft: Radius.circular(90),
-                            bottomRight: Radius.circular(50),
-                            topRight: Radius.circular(50),
-                          ),
-                          color: KRegesterButtoncolor,
+                  const SizedBox(height: 20),
+                  Column(
+                    children: [
+                      const Text(
+                        'تسجيل الدخول ',
+                        style: TextStyle(
+                          fontSize: 34,
+                          fontWeight: FontWeight.w900,
+                          fontFamily: 'Tajawal',
+                          color: KBlueColor,
                         ),
-                        child: const Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Text(
-                              'رقم الهاتف',
-                              style: TextStyle(
-                                  fontSize: 22,
-                                  fontFamily: 'Tajawal',
-                                  fontWeight: FontWeight.w900,
-                                  color: KBlueColor),
-                              textAlign: TextAlign.center,
-                            ),
-                            SizedBox(
-                              width: 20,
-                            ),
-                            Icon(Icons.phone_android,
-                              color:KBlueColor,
-                              size: 40,
-                            ),
-                          ],
-                        )),
-                  ),
-                  const SizedBox(
-                    height: 40,
-                  ),
-                  GestureDetector(
-                    onTap: (){
-                      Navigator.pushNamed(context,LoginForm.id );
-                    },
-                    child: Container(
-                        width: 270,
-                        height: 60,
-                        decoration: const BoxDecoration(
-                          borderRadius: BorderRadius.only(
-                            topLeft: Radius.circular(50),
-                            bottomRight: Radius.circular(90),
-                            bottomLeft: Radius.circular(50),
-                          ),
-                          color: KLOGINBUTTONCOLOR,
+                        textAlign: TextAlign.center,
+                      ),
+                      const Text(
+                        'من خلال ',
+                        style: TextStyle(
+                          fontSize: 26,
+                          fontFamily: 'Tajawal',
+                          fontWeight: FontWeight.w900,
+                          color: KBlueColor,
                         ),
-                        child: const Center(
+                        textAlign: TextAlign.center,
+                      ),
+                      const SizedBox(height: 40),
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.pushReplacementNamed(context, SecondLoginForm.id);
+                        },
+                        child: Container(
+                          width: 270,
+                          height: 60,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(90),
+                              bottomRight: Radius.circular(50),
+                              topRight: Radius.circular(50),
+                            ),
+                            color: KRegesterButtoncolor,
+                          ),
                           child: Row(
                             mainAxisAlignment: MainAxisAlignment.center,
                             children: [
                               Text(
-                                'البريد الالكتروني ',
+                                'رقم الهاتف',
                                 style: TextStyle(
+                                  fontSize: 22,
+                                  fontFamily: 'Tajawal',
+                                  fontWeight: FontWeight.w900,
+                                  color: KBlueColor,
+                                ),
+                                textAlign: TextAlign.center,
+                              ),
+                              SizedBox(width: 20),
+                              Icon(
+                                Icons.phone_android,
+                                color: KBlueColor,
+                                size: 40,
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 40),
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.pushNamed(context, LoginForm.id);
+                        },
+                        child: Container(
+                          width: 270,
+                          height: 60,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(50),
+                              bottomRight: Radius.circular(90),
+                              bottomLeft: Radius.circular(50),
+                            ),
+                            color: KLOGINBUTTONCOLOR,
+                          ),
+                          child: Center(
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(
+                                  'البريد الالكتروني ',
+                                  style: TextStyle(
                                     fontSize: 22,
                                     fontFamily: 'Tajawal',
                                     fontWeight: FontWeight.bold,
-                                    color: Colors.white),
-                                textAlign: TextAlign.center,
-                              ),
-                              SizedBox(
-                                width: 10,
-                              ),
-                              Icon(
-                                Icons.email,
-                                color: Colors.white,
-                                size: 40,
-                              )
-                            ],
+                                    color: Colors.white,
+                                  ),
+                                  textAlign: TextAlign.center,
+                                ),
+                                SizedBox(width: 10),
+                                Icon(
+                                  Icons.email,
+                                  color: Colors.white,
+                                  size: 40,
+                                ),
+                              ],
+                            ),
                           ),
-                        )),
-                  ),
-                  const SizedBox(
-                    height: 30,
-                  ),
-                  Container(
-                      width: 150,
-                      height: 60,
-                      decoration:const  BoxDecoration(
-                        borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(190),
-                          bottomRight: Radius.circular(190),
-                          bottomLeft: Radius.circular(190),
-                          topRight: Radius.circular(190),
                         ),
-                        color: Colors.white,
-
                       ),
-                      child: const Center(
-                        child: Text('الدخول كزائر',
-                          style: TextStyle(
+                      const SizedBox(height: 30),
+                      Container(
+                        width: 150,
+                        height: 60,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(190),
+                          color: Colors.white,
+                        ),
+                        child: Center(
+                          child: Text(
+                            'الدخول كزائر',
+                            style: TextStyle(
                               fontSize: 24,
                               fontFamily: 'Tajawal',
                               fontWeight: FontWeight.bold,
-                              color: KBlueColor),
-                          textAlign: TextAlign.center,
+                              color: KBlueColor,
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
                         ),
-                      )
+                      ),
+                      const SizedBox(height: 20),
+                      const CustomORWidget(),
+                      const SizedBox(height: 20),
+                      const CustomSocialMediaIcons(),
+                    ],
                   ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  const CustomORWidget(),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  const CustomSocialMediaIcons(),
-                ]),
-              ],
+                ],
+              ),
             ),
           ),
         ),
